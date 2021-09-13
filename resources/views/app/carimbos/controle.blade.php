4ad9cf7b1a98eca3b315feb09a571329c46fab13
Janderson <janderson.rocha97@gmail.com>
2021-09-12 23:37:14 -0300

    Formulário carimbo escalonamento crise

--- resources/views/app/carimbos/controle.blade.php
+++ resources/views/app/carimbos/controle.blade.php
@@ -18,7 +18,7 @@
             <div class="col-md-12">
                 @foreach ( $lista_carimbos_controle as $option )
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="tipo_carimbo_id" id="{{$option['tipo_carimbo']}}" value="{{ $option['id'] }}">
+                    <input class="form-check-input" type="radio" name="tipo_atividade_id" id="{{$option['tipo_carimbo']}}" value="{{ $option['id'] }}">
                     <label class="form-check-label" for="{{$option['tipo_carimbo']}}">
                         {{ $option['tipo_carimbo'] }}
                     </label>
@@ -40,6 +40,7 @@
                         @endforeach
                     </select>
                 </div>
+
                 <div class="my-2">
                     <b>CIRE ATENTO</b>
                     <br>
@@ -98,8 +99,9 @@
                             <option value="não possui caixa postal">Sem caixa postal</option>
                         </select>
                     </div>
+
                     <div class="my-2">
-                        <b>EPS:</b>
+                        <b>EPS</b>
                         <br>
                         <div class="form-group row">
                             <label for="nome_coordenacao_eps" class="col-form-label col-lg-2">Coordenador(a):</label>
@@ -124,6 +126,106 @@
                             </select>
                         </div>
                     </div>
+
+                    <div class="my-2">
+                        <b>REDE EXTERNA</b>
+                        <br>
+                        <div class="form-group row">
+                            <label for="nome_coordenacao_rede_externa" class="col-form-label col-lg-2">Coordenador(a):</label>
+                            <input type="text" class="form-control mr-lg-1 col-lg-5" name="nome_coordenacao_rede_externa" id="nome_coordenacao_rede_externa" placeholder="Coordenador(a) (REDE EXTERNA)" required>
+                            <select class="custom-select col-lg-2" name="forma_contato_coordenacao_rede_externa" required>
+                                <option selected>Canal</option>
+                                <option value="via caixa postal">Caixa postal</option>
+                                <option value="via fone">Fone</option>
+                                <option value="via telegram">Telegram</option>
+                                <option value="não possui caixa postal">Sem caixa postal</option>
+                            </select>
+                        </div>
+                        <div class="form-group row">
+                            <label for="nome_gerente_secao_rede_externa" class="col-form-label col-lg-2">Gerente Seção:</label>
+                            <input type="text" class="form-control mr-lg-1 col-lg-5" name="nome_gerente_secao_rede_externa" id="nome_gerente_secao_rede_externa" placeholder="Gerente Seção (REDE EXTERNA)">
+                            <select class="custom-select col-lg-2" name="forma_contato_gerente_secao_rede_externa" required>
+                                <option selected>Canal</option>
+                                <option value="via caixa postal">Caixa postal</option>
+                                <option value="via fone">Fone</option>
+                                <option value="via telegram">Telegram</option>
+                                <option value="não possui caixa postal">Sem caixa postal</option>
+                            </select>
+                        </div>
+                        <div class="form-group row">
+                            <label for="nome_gerente_divisao_rede_externa" class="col-form-label col-lg-2">Gerente Divisão:</label>
+                            <input type="text" class="form-control mr-lg-1 col-lg-5" name="nome_gerente_divisao_rede_externa" id="nome_gerente_divisao_rede_externa" placeholder="Gerente Divisão (REDE EXTERNA)" required>
+                            <select class="custom-select col-lg-2" name="forma_contato_gerente_divisao_rede_externa" required>
+                                <option selected>Canal</option>
+                                <option value="via caixa postal">Caixa postal</option>
+                                <option value="via fone">Fone</option>
+                                <option value="via telegram">Telegram</option>
+                                <option value="não possui caixa postal">Sem caixa postal</option>
+                            </select>
+                        </div>
+                        <div class="form-group row">
+                            <label for="nome_diretor_rede_externa" class="col-form-label col-lg-2">Diretor:</label>
+                            <input type="text" class="form-control mr-lg-1 col-lg-5" name="nome_diretor_rede_externa" id="nome_diretor_rede_externa" placeholder="Diretor (REDE EXTERNA)">
+                            <select class="custom-select col-lg-2" name="forma_contato_diretor_rede_externa" required>
+                                <option selected>Canal</option>
+                                <option value="via caixa postal">Caixa postal</option>
+                                <option value="via fone">Fone</option>
+                                <option value="via telegram">Telegram</option>
+                                <option value="não possui caixa postal">Sem caixa postal</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="my-2">
+                        <b>CIRE VIVO</b>
+                        <br>
+                        <div class="form-group row">
+                            <label for="nome_gestor_cire_vivo" class="col-form-label col-lg-2">Gestor:</label>
+                            <input type="text" class="form-control mr-lg-1 col-lg-5" name="nome_gestor_cire_vivo" id="nome_gestor_cire_vivo" placeholder="Gestor (CIRE VIVO)" required>
+                            <select class="custom-select col-lg-2" name="forma_contato_gestor_cire_vivo" required>
+                                <option selected>Canal</option>
+                                <option value="via caixa postal">Caixa postal</option>
+                                <option value="via fone">Fone</option>
+                                <option value="via telegram">Telegram</option>
+                                <option value="não possui caixa postal">Sem caixa postal</option>
+                            </select>
+                        </div>
+                        <div class="form-group row">
+                            <label for="nome_coordenacao_cire_vivo" class="col-form-label col-lg-2">Coordenador(a):</label>
+                            <input type="text" class="form-control mr-lg-1 col-lg-5" name="nome_coordenacao_cire_vivo" id="nome_coordenacao_cire_vivo" placeholder="Coordenador(a) (CIRE VIVO)">
+                            <select class="custom-select col-lg-2" name="forma_contato_coordenacao_cire_vivo" required>
+                                <option selected>Canal</option>
+                                <option value="via caixa postal">Caixa postal</option>
+                                <option value="via fone">Fone</option>
+                                <option value="via telegram">Telegram</option>
+                                <option value="não possui caixa postal">Sem caixa postal</option>
+                            </select>
+                        </div>
+                        <div class="form-group row">
+                            <label for="nome_gerente_cire_vivo" class="col-form-label col-lg-2">Gerente:</label>
+                            <input type="text" class="form-control mr-lg-1 col-lg-5" name="nome_gerente_cire_vivo" id="nome_gerente_cire_vivo" placeholder="Gerente (CIRE VIVO)" required>
+                            <select class="custom-select col-lg-2" name="forma_contato_gerente_cire_vivo" required>
+                                <option selected>Canal</option>
+                                <option value="via caixa postal">Caixa postal</option>
+                                <option value="via fone">Fone</option>
+                                <option value="via telegram">Telegram</option>
+                                <option value="não possui caixa postal">Sem caixa postal</option>
+                            </select>
+                        </div>
+                        <div class="form-group row">
+                            <label for="nome_gerente_divisao_cire_vivo" class="col-form-label col-lg-2">Gerente Divisão:</label>
+                            <input type="text" class="form-control mr-lg-1 col-lg-5" name="nome_gerente_divisao_cire_vivo" id="nome_gerente_divisao_cire_vivo" placeholder="Gerente Divisão (CIRE VIVO)">
+                            <select class="custom-select col-lg-2" name="forma_contato_gerente_divisao_cire_vivo" required>
+                                <option selected>Canal</option>
+                                <option value="via caixa postal">Caixa postal</option>
+                                <option value="via fone">Fone</option>
+                                <option value="via telegram">Telegram</option>
+                                <option value="não possui caixa postal">Sem caixa postal</option>
+                            </select>
+                        </div>
+                    </div>
+                    <button type="submit" id="btnEnviar" class="btn btn-success my-1">Gerar Carimbo</button>
+                    <button type="reset" id="btnReset" class="btn btn-danger my-1">Limpar</button>
                 </div>
             </form>
         </div>
