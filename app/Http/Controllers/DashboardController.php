00c8bcb94386a3ad33b821de040a05bfd7187d84
Janderson <janderson.rocha97@gmail.com>
2021-09-20 14:38:10 -0300

    Google chart no dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,12 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Models\Atividade;
 
 class DashboardController extends Controller
 {
     public function index() {
-        $atividades = Atividade::all();
-        return view('app.dashboard', ['title' => 'Dashboard', 'atividades' => $atividades]);
+
+        //Retornar o total de atividades
+        $total_atividades = Atividade::count();
+
+        //Retornar o total de atividades do tipo Escalonamento Crise (controle)
+        $total_escalonamento_crise = DB::table('cire_backbone_atividades')
+        ->join('cire_backbone_tipos_atividades', 'cire_backbone_atividades.tipo_atividade_id', '=', 'cire_backbone_tipos_atividades.id')
+        ->select('cire_backbone_atividades.*')
+        ->count();
+
+        return view('app.dashboard',[
+            'title' => 'Dashboard',
+            'total_atividades' => $total_atividades,
+            'total_escalonamento_crise' => $total_escalonamento_crise
+        ]);
     }
 }
--- resources/views/app/dashboard.blade.php
+++ resources/views/app/dashboard.blade.php
@@ -11,26 +11,45 @@
 
         <h4>Dashboard</h4>
         <hr>
-
-        Página em manutenção. <br><br>
-
+        
         <div class="row">
-            <div class="col-md-4">
-
-                <div class="card bg-light text-dark mb-1">
-                    <div class="card-header">
-                        Total
-                    </div>
-                    <div class="card-body">
-                        <h5 class="card-title" id="total_atividades">{{count($atividades)}}</h5>
-                    </div>
-                </div>
-
-            </div>
+          <div class="col-12">
+            <div id="piechart"></div>
+          </div>
         </div>
+
 
     </div>
 </div>
 
 @include('layouts.footer')
+
+<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+  <script type="text/javascript">
+
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+
+      var data = google.visualization.arrayToDataTable([
+        ['Tipo de atividade', 'Total'],
+        ['Escalonamento Crise', {{$total_escalonamento_crise}}]
+      ]);
+
+      var options = {
+        title: 'Atividades'
+      };
+
+      var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+      chart.draw(data, options);
+    }
+
+    $(window).resize(function(){
+      drawChart();
+    });
+
+  </script>
+
 @endsection
