d05711ccdd16066ec122d90dfa70931b9be47835
Janderson <janderson.rocha97@gmail.com>
2021-09-23 23:52:02 -0300

    Criação do model AtualizacaoTelegram

---
+++ app/Models/AtualizacaoTelegram.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AtualizacaoTelegram extends Model
+{
+    use HasFactory;
+
+    protected $table = 'cire_backbone_atualizacoes_telegram';
+
+    protected $fillable = [
+        'numero_ta',
+        'tipo_bilhete',
+        'rota_ponta_a',
+        'rota_ponta_b',
+        'trecho_ponta_a',
+        'trecho_ponta_b',
+        'possui_draco',
+        'equipamentos_v1',
+        'equipamentos_v2',
+        'operadoras',
+        'afetacao_voz',
+        'afetacao_speedy',
+        'afetacao_clientes',
+        'lp',
+        'ttmc',
+        'status',
+        'escalonamento'
+    ];
+
+    public $timestamps = false;
+}
